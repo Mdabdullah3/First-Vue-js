9399d599c0ff0485adeaa927e1b278c2c0fc314e
Mdabdullah3 <mdabdullahnoman777@gmail.com>
2024-03-10 15:03:32 +0600

    navbar and footer desing

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,14 @@
 <script setup>
 import { RouterView } from 'vue-router'
+import NavbarLayout from './components/layout/NavbarLayout.vue';
+import FooterLayout from './components/layout/FooterLayout.vue';
 </script>
 
 <template>
-  <RouterView />
+  <NavbarLayout  />
+  <div class="h-screen">
+    <RouterView />
+  </div>
+  <FooterLayout />
 </template>
->
+
