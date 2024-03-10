1926a84d8b10ca8cde0a9a06a35276603b651d1c
Mdabdullah3 <mdabdullahnoman777@gmail.com>
2024-03-10 14:32:32 +0600

    integrate tailwind css

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import AboutView from '@/views/AboutView.vue'
+import HomeView from '@/views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -12,7 +13,7 @@ const router = createRouter({
     {
       path: '/about',
       name: 'about',
-      component: () => import('../views/AboutView.vue')
+      component: AboutView
     }
   ]
 })
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,20 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+<template>
+  <div>
+ <h1 class="bg-green-500">THis is Vue js </h1>
+ <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque eos natus dicta explicabo, officiis est nesciunt maxime necessitatibus autem dolorem!</p>
+  </div>
+</template>
+
+<script>
+export default {
+  setup () {
+    
+
+    return {}
+  }
+}
 </script>
 
-<template>
-  <main>
-    <TheWelcome />
-  </main>
-</template>
+<style lang="scss" scoped>
+
+</style>
